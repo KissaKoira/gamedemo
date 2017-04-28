7ec148a836937fe592229852138ba80fbacfeb56
Onni <onni.heinonen98@gmail.comgit config --global user.name onni>
2017-04-28 13:47:40 +0300

    testailua ja pieniä muutoksia

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function init(){
+window.onload = function() {
+
   // module aliases
   var Engine = Matter.Engine,
       Render = Matter.Render,
@@ -7,28 +8,30 @@ function init(){
 
   // create an engine
   var engine = Engine.create();
+      world = engine.world
 
   // create a renderer
   var render = Render.create({
-      element: document.body,
-      engine: engine
+    element: document.body,
+    engine: engine,
+    options: {
+      width: 400,
+      height: 800,
+    }
   });
 
-  // create two boxes and a ground
-  var boxA = Bodies.rectangle(400, 200, 80, 80);
-  var boxB = Bodies.rectangle(450, 50, 80, 80);
-  var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+  //var box = Bodies.rectangle(x, y, w, h, { isStatic: true });
+  var bodies = [
+    Bodies.rectangle(350, 500, 20, 600, { isStatic: true })
+  ]
 
   // add all of the bodies to the world
-  World.add(engine.world, [boxA, boxB, ground]);
+  World.add(world, bodies);
 
   // run the engine
   Engine.run(engine);
 
   // run the renderer
   Render.run(render);
-}
 
-window.onload = function() {
-  init();
 };
